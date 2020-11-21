38ea9ca751236d3651fd88a35bf2d9124457ab7f
Anders F Björklund <anders.f.bjorklund@gmail.com>
2020-11-21 19:58:59 +0100

    Use KVM where available

--- qemu-amd64.sh
+++ qemu-amd64.sh
@@ -1,1 +1,7 @@
-qemu-system-x86_64 -M pc -nographic -smp 1 -kernel output/images/bzImage -drive file=output/images/rootfs.ext2,if=virtio,format=raw -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net user
+#!/bin/sh
+if [ -e /dev/kvm -a "`arch`" = "x86_64" ]; then
+  options="-enable-kvm"
+else
+  options=""
+fi
+qemu-system-x86_64 -M pc $options -nographic -smp 1 -kernel output/images/bzImage -drive file=output/images/rootfs.ext2,if=virtio,format=raw -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net user
--- qemu-arm64.sh
+++ qemu-arm64.sh
@@ -1,1 +1,7 @@
-qemu-system-aarch64 -M virt -cpu cortex-a53 -nographic -smp 1 -kernel output/images/Image -append "rootwait root=/dev/vda console=ttyAMA0" -netdev user,id=eth0 -device virtio-net-device,netdev=eth0 -drive file=output/images/rootfs.ext4,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0
+#!/bin/sh
+if [ -e /dev/kvm -a "`arch`" = "aarch64" ]; then
+  options="-enable-kvm -cpu host -machine gic-version=3"
+else
+  options="-cpu cortex-a53"
+fi
+qemu-system-aarch64 -M virt $options -nographic -smp 1 -kernel output/images/Image -append "rootwait root=/dev/vda console=ttyAMA0" -netdev user,id=eth0 -device virtio-net-device,netdev=eth0 -drive file=output/images/rootfs.ext4,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0
