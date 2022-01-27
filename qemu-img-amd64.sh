875ecdf487ccce69160eb9b818989de58ba5c230
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-01-28 00:00:02 +0100

    Add customized img targets for disk image
    Allows for customizing the upstream targets.
    
    Make "img" more similar to "iso", both tarch.

--- qemu-img-amd64.sh
+++ qemu-img-amd64.sh
@@ -4,4 +4,4 @@ if [ -e /dev/kvm -a "`arch`" = "x86_64" ]; then
 else
   options=""
 fi
-qemu-system-x86_64 -M pc $options -nographic -smp 1 -kernel output/images/bzImage -drive file=output/images/rootfs.ext2,if=virtio,format=raw -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net user
+qemu-system-x86_64 -M pc $options -nographic -smp 1 -kernel output/images/bzImage -drive file=output/images/rootfs.ext2,media=disk,format=raw -append "rootwait root=/dev/sda console=tty1 console=ttyS0" -net nic,model=e1000 -net user
--- run-img-amd64.sh
+++ run-img-amd64.sh
@@ -1,9 +1,8 @@
 qemu-system-x86_64 \
 	-M pc \
         -cpu max \
-	-bios /usr/share/OVMF/OVMF_CODE.fd \
 	-smp 1 \
 	-m 512 \
-	-drive file=output/images/disk-amd64.img,if=virtio,format=raw \
+	-drive file=output/images/disk-amd64.img,media=disk,format=raw \
 	-net nic,model=virtio \
 	-net user
