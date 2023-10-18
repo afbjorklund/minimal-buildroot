fc6891718502af9ce90d1ed4d23c706a0c6bdb0c
Anders F Björklund <anders.f.bjorklund@gmail.com>
2023-10-18 20:28:46 +0200

    Revert "Run with kvm when available and native arch"
    This reverts commit 769da59008bb1917cb60f76cdb064fabdc66e13a.

--- run-img-amd64.sh
+++ run-img-amd64.sh
@@ -1,15 +1,9 @@
-if [ -e /dev/kvm -a "`arch`" = "x86_64" ]; then
-  accel="kvm"
-else
-  accel="tcg"
-fi
 qemu-system-x86_64 \
 	-M pc \
         -cpu max \
 	-bios /usr/share/OVMF/OVMF_CODE.fd \
 	-smp 1 \
 	-m 512 \
-	-accel $accel \
 	-drive file=output/images/disk-amd64.img,if=virtio,media=disk,format=raw,id=hd0 \
 	-net nic,model=virtio \
 	-net user
--- run-iso-amd64.sh
+++ run-iso-amd64.sh
@@ -1,15 +1,9 @@
-if [ -e /dev/kvm -a "`arch`" = "x86_64" ]; then
-  accel="kvm"
-else
-  accel="tcg"
-fi
 qemu-system-x86_64 \
 	-M pc \
         -cpu max \
 	-bios /usr/share/OVMF/OVMF_CODE.fd \
 	-smp 1 \
 	-m 512 \
-	-accel $accel \
 	-drive file=output/images/boot-amd64.iso,if=virtio,media=cdrom,id=cd0 \
         -device virtio-scsi-pci,id=cd0 \
 	-netdev user,id=eth0 \
