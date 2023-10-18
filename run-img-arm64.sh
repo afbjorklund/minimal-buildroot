91f1d929622699072660a6658dfcb0d52acc5516
Anders F Björklund <anders.f.bjorklund@gmail.com>
2023-10-18 23:19:35 +0200

    Use same format for the firmware directory

--- run-img-arm64.sh
+++ run-img-arm64.sh
@@ -1,7 +1,7 @@
 qemu-system-aarch64 \
         -M virt \
         -cpu cortex-a53 \
-        -bios /usr/share/qemu-efi-aarch64/QEMU_EFI.fd \
+        -bios /usr/share/AAVMF/AAVMF_CODE.fd \
 	-smp 1 \
         -m 512 \
         -drive file=output/images/disk-arm64.img,if=none,format=raw,id=hd0 \
--- run-iso-arm64.sh
+++ run-iso-arm64.sh
@@ -1,7 +1,7 @@
 qemu-system-aarch64 \
         -M virt \
         -cpu cortex-a53 \
-        -bios /usr/share/qemu-efi-aarch64/QEMU_EFI.fd \
+        -bios /usr/share/AAVMF/AAVMF_CODE.fd \
 	-smp 1 \
 	-m 512 \
 	-drive file=output/images/boot-arm64.iso,if=virtio,media=cdrom,id=cd0 \
